0b8bdf55f8068b3c3f5fa9d7b129399d40995f2a
Pernille Johnsen <pernillej11@gmail.com>
2018-09-18 12:58:13 +0200

    Setup import capabilities from gann file til main.

--- Module 1/src/main.py
+++ Module 1/src/main.py
@@ -1,4 +1,4 @@
-
+from .gann import Gann, GannLayer, Case
 
 def main(dimensions, hidden_activation_function, output_activation_function, cost_function, learning_rate,
          init_weight_range, optimizer, data_source, case_fraction, validation_fraction, validation_interval,
