374aca68a4aab26b66248b9c82ba55848bea2ec2
Pernille Johnsen <pernillej11@gmail.com>
2018-10-18 16:27:10 +0200

    Added basic nim game functions.

--- Module 2/src/nimsimulator.py
+++ Module 2/src/nimsimulator.py
@@ -1,4 +1,4 @@
-from enum import Enum
+import random
 
 """
 Given N and K, the remaining rules of play are extremely simple: Players take turns removing pieces, 
@@ -29,14 +29,74 @@ class NimSimulator:
         :param K: Maximum number of pieces that either player can take on their turn
         :param verbose: Verbose mode, if true details of the game are printed (default = False)
         """
-        return
+        self.G = G
+        self.player = self.set_start_player(P)
+        self.M = M
+        self.N = N
+        self.K = K
+        self.verbose = verbose
 
+        self.finished = False
 
-class POptions(Enum):
-    """
-    Enum for starting player options
-    """
+        if verbose:
+            print("Created new Nim game with N = " + str(self.N) + " and K = " + str(self.K) +
+                  ". Player " + str(self.player + 1) + " starts.")
 
-    Player1 = 0
-    Player2 = 1
-    Mix = 2
+    def remove_pieces(self, pieces):
+        """
+        Removes selected amount of pieces from pile
+
+        :param pieces: Number pieces to remove
+        """
+        if pieces < 1 or pieces > self.K or pieces > self.N:
+            print("Incompatible amount of pieces")
+        else:
+            self.N = self.N - pieces
+            if self.verbose:
+                print("Player " + str(self.player + 1) + " selects " + str(pieces) + " pieces: Remaining pieces = "
+                      + str(self.N))
+
+    def select_pieces(self, pieces):
+        """
+        Plays one move by a player by removing pieces, checking if finished and changing player at the end
+
+        :param pieces: Number of pieces to remove
+        """
+        self.remove_pieces(pieces)
+        self.finished = self.check_win()
+        self.change_player()
+
+    def check_win(self):
+        """
+        Checks if game is won
+
+        :return: True is win, False if not
+        """
+        if self.N == 0:
+            if self.verbose:
+                print("Player " + str(self.player + 1) + " won ")
+            return True
+        else:
+            return False
+
+    def is_finished(self):
+        return self.finished
+
+    def change_player(self):
+        self.player = (self.player + 1) % 2
+
+    @staticmethod
+    def set_start_player(P):
+        """
+        Sets start player based on user input P
+
+        :param P: Option for player to start
+        :return: Number of player who starts
+        """
+        if P == "p1":
+            return 0
+        elif P == "p2":
+            return 1
+        else:
+            return random.randint(0, 1)
+
