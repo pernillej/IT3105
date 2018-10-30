b417ad7c58c4520281c668677a9b572fd371649c
Pernille Johnsen <pernillej11@gmail.com>
2018-10-30 16:28:35 +0100

    Changed main interface stup.

--- Module 2/src/main.py
+++ Module 2/src/main.py
@@ -1,24 +1,16 @@
+from Simulator import NimSimulator
 
-
-def main(M, G, P, N, K, verbose=False):
-    """
-    Main function to setup user specified scenario parameters, setup game and run Monte Carlo Tree Search
-
-    :param M: Number of simulations
-    :param G: Number of games in a batch
-    :param P: Starting-player option
-    :param N: Starting number of pieces/stones in each game
-    :param K: Maximum number of pieces that either player can take on their turn
-    :param verbose: Verbose mode, if true details of the game are printed (default = False)
-    """
-    return
-
+""" Main interface, specifying user parameters """
 
 if __name__ == '__main__':
-    M = 100
-    G = 10
-    P = 1
-    N = 10
-    K = 3
-    verbose = False
-    main(M, G, P, N, K, verbose=verbose)
+    user_parameters = {
+        "g": 0,
+        "p": 0,
+        "m": 0,
+        "n": 0,
+        "k": 0,
+        "verbose": False
+    }
+
+    sim = NimSimulator()
+    sim.simulate_batch(**user_parameters)
